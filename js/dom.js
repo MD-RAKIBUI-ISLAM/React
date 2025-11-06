7a3db94ffcc73d6b84cfe6321ff7f20bbc3db7a7
MD-RAKIBUL-ISLAM <md.rakibul2828@gmail.com>
2025-11-06 15:43:49 +0600

    Updated

--- js/dom.js
+++ js/dom.js
@@ -1,14 +1,14 @@
-let array = [];
-increment = 0;
-let container = document.querySelector('.container');
+// let array = [];
+// increment = 0;
+// let container = document.querySelector('.container');
 
 //fast
 
-while(increment < 10000) {
-    array.push(++increment);
-}
+// while(increment < 10000) {
+//     array.push(++increment);
+// }
 
-container.innerHTML = array.join(' ');
+// container.innerHTML = array.join(' ');
 
 //slow
 
@@ -16,3 +16,34 @@ container.innerHTML = array.join(' ');
 //     increment++;
 //     container.innerHTML += ' ' + increment;
 // }
+
+
+/*
+ * Title: React JS File
+ * Description: A React JS Implementation of efficient DOM manipulation
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 02/08/2021
+ *
+ */
+
+const domContainer = document.querySelector("#root");
+
+const Fruits = () => {
+  const [fruit, setFruit] = React.useState('');
+  const [fruits, setFruits] = React.useState(['mango', 'guava', 'apple', 'oragne']);
+  
+  return (
+    <div className="container">
+      <ul id="fruits">
+        {fruits.sort().map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
+      <br/>
+      <p><input type="text" value={fruit} onChange={(e) => setFruit(e.target.value)} /></p>
+      <button onClick={() => setFruits([...fruits, fruit])}>Add Item</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Fruits />, domContainer);
