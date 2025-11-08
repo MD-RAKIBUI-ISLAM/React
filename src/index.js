9c69d00c4ba1c8c52fcbe966c6077954fc0fa770
MD-RAKIBUL-ISLAM <md.rakibul2828@gmail.com>
2025-11-09 02:20:28 +0600

    Try to understand React JSX elements and Rendering elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const index = 0;
+setInterval(() => {
+    const element = (
+        <h1 className="text" tabIndex={index}>
+            <span className="text">Hello {new Date().toLocaleTimeString()} </span>
+            <img src="" alt="" />
+        </h1>
+    );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
-
-reportWebVitals();
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(element);
+}, 1000);
