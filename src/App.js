35db395391cd0bad35a66a066bf3cb450ea7b988
MD-RAKIBUL-ISLAM <md.rakibul2828@gmail.com>
2025-11-12 12:58:44 +0600

    Learned Event handling and Control Re Rendering

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Clock from './Components/Clock';
+// import Counter from './Components/Counter';
+
+function App() {
+    return (
+        <div>
+            <Clock />
+            {/* <Counter /> */}
+        </div>
+    );
+}
+
+export default App;
